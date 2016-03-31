6c0ae7e1bd4d84d45a9403f9947debec71ecdb17
nguyễn tấn tu <tunguyen.it.dev@gmail.com>
2016-04-01 01:42:23 +0700

    new website

---
+++ WebDuLich/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using WebDuLich.Domain;
+using WebDuLich.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDuLich.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
