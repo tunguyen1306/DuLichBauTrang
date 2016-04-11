077b35aae307cb1a7d5d527c1f3358f083471b77
tu nguyen <tu nguyen>
2016-04-11 22:48:54 +0700

    no message

--- WebDuLich/Controllers/DefaultController.cs
+++ WebDuLich/Controllers/DefaultController.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebDuLich.Models;
 
 namespace WebDuLich.Controllers
 {
     public class DefaultController : Controller
     {
+        private WebDuLichEntities db = new WebDuLichEntities();
         // GET: Default
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Slide()
+        {
+            var qs = from dataslide in db.tbl_slide_dulich
+                     where dataslide.trangthai_silde_dulich == 1
+                     select dataslide;
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            
+            return View();
+        }
+        public ActionResult TieuBieu()
+        {
+
+            return View();
+        }
+        public ActionResult Header(int id)
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+
+            return View();
+        }
+
+
     }
 }
